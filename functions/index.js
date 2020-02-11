dfe8facd1f6060769a40fbeeb66fb627b7156cde
Noah F <franck.noahk@gmail.com>
2020-02-11 12:46:18 -0600

    Added function for addUserToDB.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+const db = admin.firestore();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+exports.addUserToDB = functions.auth.user().onCreate((user) => {
+	db.doc('users/' + user.uid).set({
+		email       : user.email,
+		name        : user.displayName,
+		isAdmin     : false,
+		isFollowing : [],
+	});
+});
