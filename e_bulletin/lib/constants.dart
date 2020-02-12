fea8bd3b9552ab3dac2b9853efa07defef426755
Noah F <franck.noahk@gmail.com>
2020-02-12 16:18:32 -0600

    Added a set of constants that can be accessed anywehre.

---
+++ e_bulletin/lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:e_bulletin/widgets/layout/SettingsLayout.dart';
+import 'package:e_bulletin/widgets/layout/UpCommingEventsList.dart';
+import 'package:flutter/material.dart';
+
+int screen = 0;
+
+List<Widget> layoutWidgetArr = [
+  UpcommingEventsList(),
+  SettingsLayout(),
+];
