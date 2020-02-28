9239f0a3bfa2ca5bf88f5d37e95fd6b0cf2360df
Noah F <franck.noahk@gmail.com>
2020-02-28 12:07:49 -0600

    Change imports.

--- e_bulletin/lib/main.dart
+++ e_bulletin/lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
 import 'package:e_bulletin/widgets/layout/UpCommingEventsList.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
