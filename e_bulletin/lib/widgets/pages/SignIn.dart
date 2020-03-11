47fd6ee3854c9d51aa1b635ff35637f77022b605
Noah-work <noah-work@glendas-MacBook-Pro.local>
2020-03-11 18:32:45 -0500

    Added form information for signing in.

--- e_bulletin/lib/widgets/pages/SignIn.dart
+++ e_bulletin/lib/widgets/pages/SignIn.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 class SignIn extends StatelessWidget {
   const SignIn({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final FirebaseAuth _auth = FirebaseAuth.instance;
-    
+    String userName = "";
+    String password = "";
     return Scaffold(
       appBar: AppBar(
-        // actions: <Widget>[
-        //   BackButton(
-        //     color: Colors.black,
-        //     onPressed: () {
-        //       Navigator.pop(context);
-        //     },
-        //   )
-        // ],
-      ),
+          // actions: <Widget>[
+          //   BackButton(
+          //     color: Colors.black,
+          //     onPressed: () {
+          //       Navigator.pop(context);
+          //     },
+          //   )
+          // ],
+          ),
       body: Center(
-        child: Text(
-          "SignIn",
+        child: Form(
+          child: Container(
+            margin: EdgeInsets.all(24),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  autofocus: true,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    hintText: "E-Mail",
+                  ),
+                  onChanged: (String changes) {
+                    userName = changes;
+                  },
+                ),
+                TextFormField(
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                  ),
+                  onChanged: (String changes) {
+                    password = changes;
+                  },
+                  obscureText: true,
+                ),
+                RaisedButton(
+                  child: Text("Submit"),
+                  color: Colors.red[700],
+                  onPressed: () {
+                    trySignIn(userName, password);
+                  },
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
+
+void trySignIn(String userName, String password) {}
