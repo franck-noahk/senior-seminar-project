7a4eda46c165621bb4ae545a0ebbb3544f44d104
Noah F <franck.noahk@gmail.com>
2020-03-22 13:20:02 -0500

    Gave the register an app bar & pop the context if pressed toggle.

--- e_bulletin/lib/widgets/pages/register.dart
+++ e_bulletin/lib/widgets/pages/register.dart
@@ -11,7 +11,18 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("E-Bulliten Register New User"),
+        backgroundColor: Colors.red[700],
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          )
+        ],
+      ),
       body: Container(
           child: Column(
         children: <Widget>[],
