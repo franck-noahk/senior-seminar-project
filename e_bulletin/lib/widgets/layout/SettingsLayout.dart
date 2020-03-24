76fd05a32b47272e9a56df202551d12d6d6906e2
Noah F <franck.noahk@gmail.com>
2020-03-24 15:37:38 -0500

    Trying to access the data from settings but running into issues.

--- e_bulletin/lib/widgets/layout/SettingsLayout.dart
+++ e_bulletin/lib/widgets/layout/SettingsLayout.dart
@@ -1,14 +1,23 @@
+import 'package:e_bulletin/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SettingsLayout extends StatelessWidget {
+class SettingsLayout extends StatefulWidget {
   const SettingsLayout({Key key}) : super(key: key);
 
   @override
+  _SettingsLayoutState createState() => _SettingsLayoutState();
+}
+
+class _SettingsLayoutState extends State<SettingsLayout> {
+  @override
   Widget build(BuildContext context) {
-    print("Testing");
+    var user = Provider.of<User>(context);
     return Center(
       key: Key("TESTING"),
-      child: Text("Testing"),
+      child: Text(
+        (user.email == null) ? "it is null" : user.email,
+      ),
     );
   }
 }
