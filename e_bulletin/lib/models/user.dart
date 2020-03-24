751340017d9c0ba34f4c567ff816dda9db0d9f6c
Noah F <franck.noahk@gmail.com>
2020-03-24 15:36:51 -0500

    Added ability for users to access their document in db.

--- e_bulletin/lib/models/user.dart
+++ e_bulletin/lib/models/user.dart
@@ -1,8 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_bulletin/backend/firebase.dart';
+
 class User {
   final String uid;
   String email;
   bool isAdmin;
   String name;
+  FStoredb db;
+  List<String> isFollowing = new List<String>();
 
-  User({this.uid}) {}
+  User({this.uid}) {
+    db = new FStoredb(uid: this.uid);
+  }
+
+  Future<void> getDataDb() async {
+    try {
+      DocumentSnapshot result = await this.db.getData();
+      if (result == null) {
+        //Document does not exist
+      }
+      this.email = result.data['email'];
+      this.isAdmin = result.data['isAdmin'];
+      this.name = result.data['name'];
+      // this.isFollowing = result.data['isFollowing'];
+      print("\n\n\n" + this.email + "\n\n\n");
+      print(this.isFollowing);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
