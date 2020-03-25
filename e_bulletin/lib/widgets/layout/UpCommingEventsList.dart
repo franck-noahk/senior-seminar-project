db2267319c0d831d4d1731203dac9b31876bd993
Noah F <franck.noahk@gmail.com>
2020-03-25 12:50:37 -0500

    Changed the querry to only show upcomming events.

--- e_bulletin/lib/widgets/layout/UpCommingEventsList.dart
+++ e_bulletin/lib/widgets/layout/UpCommingEventsList.dart
@@ -20,38 +20,39 @@ class _UpcommingEventsListState extends State<UpcommingEventsList> {
     return StreamBuilder(
       stream: Firestore.instance
           .collection('events')
-          .orderBy(
-            'event_time',
-            descending: true,
+          .where(
+            "event_time",
+            isGreaterThan: new DateTime.now().millisecondsSinceEpoch,
           )
           .snapshots(),
       builder: (BuildContext context, snapshot) {
-        if (!snapshot.hasData && snapshot.connectionState.index == 2)
-          return Center(child: Text('Loading...'));
-        if (!snapshot.hasData && snapshot.connectionState.index == 3)
-          return Center(child: Text('No Events:'));
         if (snapshot.hasData) {
           final int messageCount = snapshot.data.documents.length;
-          return ListView.builder(
-              itemCount: messageCount,
-              itemBuilder: (_, int index) {
-                final DocumentSnapshot document =
-                    snapshot.data.documents[index];
-                final time = document['time'];
-                final dynamic message = document['name'];
-                return Card(
-                  elevation: 2.0,
-                  child: ListTile(
-                    title: Text(
-                      message != null
-                          ? message.toString()
-                          : '<No message retrieved>',
+          if (messageCount == 0) {
+            return Center(
+              child: Text("No Upcomming Events"),
+            );
+          } else
+            return ListView.builder(
+                itemCount: messageCount,
+                itemBuilder: (_, int index) {
+                  final DocumentSnapshot document =
+                      snapshot.data.documents[index];
+                  final time = document['time'];
+                  final dynamic message = document['name'];
+                  return Card(
+                    elevation: 2.0,
+                    child: ListTile(
+                      title: Text(
+                        message != null
+                            ? message.toString()
+                            : '<No message retrieved>',
+                      ),
+                      subtitle:
+                          Text(time != null ? time.toString() : "no message"),
                     ),
-                    subtitle:
-                        Text(time != null ? time.toString() : "no message"),
-                  ),
-                );
-              });
+                  );
+                });
         } else {
           return Loading();
         }
