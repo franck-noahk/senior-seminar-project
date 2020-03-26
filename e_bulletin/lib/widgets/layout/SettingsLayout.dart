c7c9139dc36c5067ecaacd96fd6213981ce78ccb
Noah F <franck.noahk@gmail.com>
2020-03-26 13:32:42 -0500

    Settings tab now shows all of the orginizations & hearts next to those that people follow.

--- e_bulletin/lib/widgets/layout/SettingsLayout.dart
+++ e_bulletin/lib/widgets/layout/SettingsLayout.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_bulletin/models/user.dart';
+import 'package:e_bulletin/widgets/layout/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,17 +10,59 @@ class SettingsLayout extends StatefulWidget {
   @override
   _SettingsLayoutState createState() => _SettingsLayoutState();
 }
+//TODO: Come up with settings to load & display. Followers and such.
 
 class _SettingsLayoutState extends State<SettingsLayout> {
   @override
   Widget build(BuildContext context) {
     var user = Provider.of<User>(context);
-    var snapshot = Provider.of<DocumentSnapshot>(context);
-    return Center(
-      key: Key("TESTING"),
-      child: Text(
-          // (data.data['email'] == null) ? "email is null" : user.email,
-          snapshot.data['email']),
+    var userData = Provider.of<DocumentSnapshot>(context);
+
+    return StreamBuilder(
+      stream: Firestore.instance.collection('orginizations').snapshots(),
+      builder: (BuildContext context, snapshot) {
+        if (!snapshot.hasData) {
+          return Loading();
+        }
+        int messageCount = snapshot.data.documents.length;
+        if (messageCount == 0) {
+          return Center(
+            child: Text("No Followers"),
+          );
+        }
+        // List<dynamic> list= userData.data['isFollowing'];
+        // list.any((element) => false)
+
+        print(userData.data['isFollowing'].toString());
+
+        return ListView.builder(
+          itemCount: messageCount,
+          itemBuilder: (_, int index) {
+            DocumentSnapshot document = snapshot.data.documents[index];
+            DocumentReference test;
+            print(document['uid'].toString());
+            return Card(
+              child: ListTile(
+                title: Text(document['name']),
+                leading: (userData.data['isFollowing']
+                        .toList()
+                        .contains(document['uid']))
+                    ? Icon(Icons.favorite)
+                    : Icon(Icons.favorite_border),
+                subtitle: Text(
+                    (document["fullName"] == null) ? "" : document["fullName"]),
+              ),
+            );
+          },
+        );
+      },
     );
+
+    // return Center(
+    //   key: Key("TESTING"),
+    //   child: Text(
+    //       // (data.data['email'] == null) ? "email is null" : user.email,
+    //       snapshot.data['email']),
+    // );
   }
 }
