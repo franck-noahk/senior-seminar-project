acc9259761778f3460612194476424ca714fc3a0
Noah F <franck.noahk@gmail.com>
2020-04-02 15:45:58 -0500

    I think this is correct function, but I haven't checked it yet

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
+const fcm = admin.messageing();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -14,10 +15,26 @@ exports.addUserToDB = functions.auth.user().onCreate((user) => {
 	//TODO: set up the teacher/authorized users.
 
 	db.doc('users/' + user.uid).set({
-		email       : user.email,
-		uid         : user.uid,
-		name        : user.displayName,
-		isAdmin     : false,
-		isFollowing : [],
+		email: user.email,
+		uid: user.uid,
+		name: user.displayName,
+		isAdmin: false,
+		isFollowing: [],
 	});
 });
+
+exports.sendNotificationToTopic = functions.firestore.document('/events/{eventID}').onCreate(async snapshot => {
+	let eventData = snapshot.data();
+	let orginizaer = await db.doc(eventData.orginizaer).get();
+	let orginizerData = organizer.data();
+	let payLoad = {
+		notification: {
+			title: "New Event Created",
+			body: `${organizerData.name} created an event ${eventData.name}`,
+			clickAction: 'FLUTTER_NOTIFICATION_CLICK'
+		}
+	};
+
+	fcm.sendNotificationToTopic(orginizerData.name, payLoad);
+
+})
