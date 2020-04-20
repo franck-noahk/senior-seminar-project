30ed173769f477b616825032cfe2a4d86ca62794
Noah F <Franck.noahk@gmail.com>
2020-04-20 10:11:05 -0500

    Auto Hides tabs & redirects user off sitest they don't need.

--- public/javascript/user.js
+++ public/javascript/user.js
@@ -4,7 +4,9 @@ firebase.auth().onAuthStateChanged(function (user) {
   //Pulling values from our db
   if (user) {
     uid = user.uid;
+    console.log(uid);
   } else {
     uid = null;
+    console.log(uid);
   }
 });
